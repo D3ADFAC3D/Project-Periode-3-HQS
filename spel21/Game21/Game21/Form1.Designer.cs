74c704bf43a042694181385705a5d5f478330492
Lars Brongers <0327344@student.rocvantwente.nl>
2021-03-24 23:02:50 +0100

    ////

---
+++ spel21/Game21/Game21/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿
+namespace Game21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnOpniew = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnfoto1 = new System.Windows.Forms.Button();
+            this.btnopslaan1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.btnfoto2 = new System.Windows.Forms.Button();
+            this.btnopslaan2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblPlayer1 = new System.Windows.Forms.Label();
+            this.lblPlayer2 = new System.Windows.Forms.Label();
+            this.picSpeler = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSpeel = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picSpeler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(538, 61);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(137, 126);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnOpniew
+            // 
+            this.btnOpniew.Location = new System.Drawing.Point(538, 193);
+            this.btnOpniew.Name = "btnOpniew";
+            this.btnOpniew.Size = new System.Drawing.Size(137, 126);
+            this.btnOpniew.TabIndex = 1;
+            this.btnOpniew.Text = "Opniew";
+            this.btnOpniew.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnopslaan1);
+            this.groupBox1.Controls.Add(this.btnfoto1);
+            this.groupBox1.Location = new System.Drawing.Point(28, 66);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(393, 296);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Speler 1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.btnopslaan2);
+            this.groupBox2.Controls.Add(this.btnfoto2);
+            this.groupBox2.Controls.Add(this.dateTimePicker2);
+            this.groupBox2.Controls.Add(this.listBox2);
+            this.groupBox2.Location = new System.Drawing.Point(805, 66);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(410, 295);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Speler 2";
+            // 
+            // btnfoto1
+            // 
+            this.btnfoto1.Location = new System.Drawing.Point(170, 158);
+            this.btnfoto1.Name = "btnfoto1";
+            this.btnfoto1.Size = new System.Drawing.Size(169, 22);
+            this.btnfoto1.TabIndex = 0;
+            this.btnfoto1.Text = "Selecteer foto";
+            this.btnfoto1.UseVisualStyleBackColor = true;
+            this.btnfoto1.Click += new System.EventHandler(this.btnfoto1_Click);
+            // 
+            // btnopslaan1
+            // 
+            this.btnopslaan1.Location = new System.Drawing.Point(170, 203);
+            this.btnopslaan1.Name = "btnopslaan1";
+            this.btnopslaan1.Size = new System.Drawing.Size(169, 30);
+            this.btnopslaan1.TabIndex = 1;
+            this.btnopslaan1.Text = "Opslaan";
+            this.btnopslaan1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Naam";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "GeboorteDatum";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(149, 82);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(149, 43);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(200, 17);
+            this.listBox1.TabIndex = 5;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(161, 43);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(200, 17);
+            this.listBox2.TabIndex = 6;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(161, 82);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 6;
+            // 
+            // btnfoto2
+            // 
+            this.btnfoto2.Location = new System.Drawing.Point(192, 158);
+            this.btnfoto2.Name = "btnfoto2";
+            this.btnfoto2.Size = new System.Drawing.Size(169, 22);
+            this.btnfoto2.TabIndex = 6;
+            this.btnfoto2.Text = "Selecteer foto";
+            this.btnfoto2.UseVisualStyleBackColor = true;
+            // 
+            // btnopslaan2
+            // 
+            this.btnopslaan2.Location = new System.Drawing.Point(192, 203);
+            this.btnopslaan2.Name = "btnopslaan2";
+            this.btnopslaan2.Size = new System.Drawing.Size(169, 30);
+            this.btnopslaan2.TabIndex = 6;
+            this.btnopslaan2.Text = "Opslaan";
+            this.btnopslaan2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "GeboorteDatum";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Naam";
+            // 
+            // lblPlayer1
+            // 
+            this.lblPlayer1.AutoSize = true;
+            this.lblPlayer1.Location = new System.Drawing.Point(195, 414);
+            this.lblPlayer1.Name = "lblPlayer1";
+            this.lblPlayer1.Size = new System.Drawing.Size(41, 13);
+            this.lblPlayer1.TabIndex = 4;
+            this.lblPlayer1.Text = "Punten";
+            // 
+            // lblPlayer2
+            // 
+            this.lblPlayer2.AutoSize = true;
+            this.lblPlayer2.Location = new System.Drawing.Point(994, 414);
+            this.lblPlayer2.Name = "lblPlayer2";
+            this.lblPlayer2.Size = new System.Drawing.Size(41, 13);
+            this.lblPlayer2.TabIndex = 5;
+            this.lblPlayer2.Text = "Punten";
+            // 
+            // picSpeler
+            // 
+            this.picSpeler.Location = new System.Drawing.Point(538, 534);
+            this.picSpeler.Name = "picSpeler";
+            this.picSpeler.Size = new System.Drawing.Size(137, 174);
+            this.picSpeler.TabIndex = 6;
+            this.picSpeler.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(698, 468);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(53, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Actief";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(584, 711);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "lblBeurt";
+            // 
+            // btnSpeel
+            // 
+            this.btnSpeel.Location = new System.Drawing.Point(538, 501);
+            this.btnSpeel.Name = "btnSpeel";
+            this.btnSpeel.Size = new System.Drawing.Size(137, 27);
+            this.btnSpeel.TabIndex = 11;
+            this.btnSpeel.Text = "Spelen!!";
+            this.btnSpeel.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(168, 440);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 116);
+            this.textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(966, 440);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 116);
+            this.textBox2.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(1247, 733);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSpeel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.picSpeler);
+            this.Controls.Add(this.lblPlayer2);
+            this.Controls.Add(this.lblPlayer1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnOpniew);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Play21";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picSpeler)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnOpniew;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnopslaan1;
+        private System.Windows.Forms.Button btnfoto1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnopslaan2;
+        private System.Windows.Forms.Button btnfoto2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label lblPlayer1;
+        private System.Windows.Forms.Label lblPlayer2;
+        private System.Windows.Forms.PictureBox picSpeler;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSpeel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ spel21/Game21/Game21/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Startup();
+
+        }
+
+        private void btnfoto1_Click(object sender, EventArgs e)
+        {
+            Image Foto1 = default;
+            Fotop1(Foto1);            
+        }
+        public void Startup() // deze methode word opgeroepen wanneer het programma word op gestart en zet alle buttons op inactief
+        {
+            btnOpniew.Enabled = false;
+            btnopslaan1.Enabled = false;
+            btnopslaan2.Enabled = false;
+            btnSpeel.Enabled = false;
+            btnStart.Enabled = false;
+        }
+
+        public void Fotop1(Image Foto1) // Deze Methode pakt de foto van speler1 
+        {
+            OpenFileDialog iFoto1 = new OpenFileDialog();
+            iFoto1.Filter = "Image Files (*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (iFoto1.ShowDialog() == DialogResult.OK)
+            {
+                Foto1 = Image.FromFile(iFoto1.FileName);
+                picSpeler.Image = Foto1;
+                picSpeler.SizeMode = PictureBoxSizeMode.StretchImage;
+                btnopslaan1.Enabled = true;
+            }
+            
+        }
+
+
+    }
+}
